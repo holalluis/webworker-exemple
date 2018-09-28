482e7c6bdf477f63d0c58003fcaaa682f6600e31
holalluis <holalluis@gmail.com>
2018-09-28 23:05:54 +0200

    tercer commit

--- worker.js
+++ worker.js
@@ -1,10 +1,9 @@
 self.addEventListener('message',function(ev){
-  if(ev.data=='calcula'){
-    console.log('calculant suma...')
-  }else{
-    console.log('ordre desconeguda')
-    return;
+  if(ev.data!='calcula'){
+    console.log('ordre desconeguda');return;
   }
+
+  console.log('calculant suma...')
   var resultat=0;
   for(var i=0;i<1e5;i++){
     resultat+=i;
