5ab84a175ce42b2b1bbe4a612146f92931de3e73
Lluís Bosch <holalluis@gmail.com>
2018-10-01 17:39:15 +0200

    mini canvi

--- worker.js
+++ worker.js
@@ -1,11 +1,11 @@
-self.addEventListener('message',function(ev){
+self.addEventListener('message',ev=>{
   if(ev.data!='calcula'){
     console.log('ordre desconeguda');return;
   }
 
   console.log('calculant suma...')
   var resultat=0;
-  for(var i=0;i<1e5;i++){
+  for(var i=0;i<1e6;i++){
     resultat+=i;
   }
   self.postMessage(resultat);
